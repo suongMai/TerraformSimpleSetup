a433f877a08d73cac9bd7aa7e8637c7ea6923837
Hoa Nguyen <suong05mt@gmail.com>
2024-04-22 19:18:29 +0700

    indent fixing
    indent fixing

--- ec2/README.md
+++ ec2/README.md
@@ -1,5 +1,5 @@
 # Chalenge
-    - 1 Create a DBB server and out put the private IP
+    - 1 Create a DB server and out put the private IP
     - 2 Create a web server and ensure  it has a fixed public IP
     - 3 Create a Security Group fro the web server opening ports 80 and 443 (HTTP adn HTTPS)
     - 4 Run the provided script on the web server
--- ec2/main.tf
+++ ec2/main.tf
@@ -48,15 +48,15 @@ resource "aws_security_group" "web_traffic" {
         }
     }
 
-        dynamic "egress" {
-        iterator = port
-        for_each = var.egress
-        content {
-            from_port = port.value
-            to_port = port. value
-            protocol = "TCP"
-            cidr_blocks = ["0.0.0.0/0"]
-        }
+    dynamic "egress" {
+    iterator = port
+    for_each = var.egress
+    content {
+        from_port = port.value
+        to_port = port. value
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     }
 }
 
